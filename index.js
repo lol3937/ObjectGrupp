3abc553b805c305b58183d82738569df2ac2f7c2
Erik <43432787+ErikLager@users.noreply.github.com>
2019-12-05 10:58:52 +0100

    Basic done

---
+++ index.js
@@ -0,0 +1,12 @@
+// Example of a object
+let bil = {
+    name: "Tesla", 
+    model: "Cybertruck", 
+    year: "2025"};
+
+// A example of another object
+let person = {
+    firstName: "Elon", 
+    lastName:"Musk", 
+    age: 50, 
+    occupation: "TeslaMaster"}
